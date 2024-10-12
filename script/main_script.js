1a8d385290c96c531379ef763a7873cc9d95c34e
FjordieFjordsen <goetze.rene@web.de>
2024-10-12 21:43:29 +0200

    added Saturday

---
+++ script/main_script.js
@@ -0,0 +1,8 @@
+function init() {
+    document.getElementById('fade-in').classList.add('fade-in');
+}
+
+
+function redirect() {
+    window.location.href = 'signup.html'; // Die Seite, auf die der Button leitet
+}
