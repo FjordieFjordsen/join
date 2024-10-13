b0843b3c62b6cbb66a44ff81f37085b5b7296943
FjordieFjordsen <goetze.rene@web.de>
2024-10-13 20:15:03 +0200

    added Sunday

--- script/main_script.js
+++ script/main_script.js
@@ -6,3 +6,8 @@ function init() {
 function redirect() {
     window.location.href = 'signup.html'; // Die Seite, auf die der Button leitet
 }
+
+
+function goBack() {
+    window.history.back(); // Geht zur vorherigen Seite im Browser-Verlauf
+}
